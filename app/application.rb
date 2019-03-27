a8068ee2834f155e3674a1fd1859d39669647d68
Joan Lyness <joanrigdon@gmail.com>
2019-03-27 02:58:32 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -47,7 +47,7 @@ class Application
       @@cart << search_term
       resp.write "added #{search_term}"
     else
-      resp.write "We don't have that item."
+      return "We don't have that item."
     end
   end
 
