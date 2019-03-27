42efadd12b32a4c7bad2516960fbfdbcf40cfc26
Joan Lyness <joanrigdon@gmail.com>
2019-03-27 02:50:38 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,9 +11,11 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+
     elsif req.path.match(/cart/)
       if @@cart.size > 0
         @@cart.each do |item|
@@ -21,6 +23,11 @@ class Application
         end
       else resp.write "Your cart is empty."
       end
+
+    elsif req.path.match(/add/)
+      seach_term = req.params["q"]
+      resp.write handle_add(search_term)
+
     else
       resp.write "Path Not Found"
     end
@@ -30,6 +37,14 @@ class Application
   def handle_search(search_term)
     if @@items.include?(search_term)
       return "#{search_term} is one of our items"
+    else
+      return "Couldn't find #{search_term}"
+    end
+  end
+
+  def handle_add(search_term)
+    if @@items.include?(search_term)
+      @@cart << search_term
     else
       return "Couldn't find #{search_term}"
     end
