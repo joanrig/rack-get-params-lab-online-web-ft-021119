e42aef74927db23a790960bf55cca3d28562afdd
Joan Lyness <joanrigdon@gmail.com>
2019-03-27 03:06:09 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -25,14 +25,14 @@ class Application
       end
 
     elsif req.path.match(/add/)
-      seach_term = req.params["item"]
+      search_term = req.params["item"]
       if @@items.include?(search_term)
         @@cart << search_term
         resp.write "added #{cart.last}"
       else
         resp.write "We don't have that item."
       end
-      
+
     else
       resp.write "Path Not Found"
     end
